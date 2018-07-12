c20f5846eac11bec980d04931188bbc98cbc0c63
ajomadlabs <ajojohn@madlabs.xyz>
2018-07-12 23:22:58 +0530

    :smiley: Done coding top header

---
+++ src/components/pageComponents/Welcome.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+        <div class="container">
+          <ul class="mhack-top-nav">
+            <li class="mhacks-tn-left-logo">
+              <a>Mad.Hacks</a>
+            </li>
+            <li class="mhacks-tn-right-item">
+              <a>Contact Us</a>
+            </li>
+            <li class="mhacks-tn-right-item">
+              <a>About Us</a>
+            </li>
+            <li class="mhacks-tn-right-item">
+              <a>Services</a>
+            </li>
+          </ul>
+        </div>
+        <div class="row">
+          <div class="container">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+              <div class="mhacks-top-header">
+                Community For All
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="container">
+            <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+              <div class="mhacks-top-para">
+                Student developer community built for and by the students to
+                enhance the skillset of students as well as to develop tech savy tools.
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="container">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+              <div class="mhacks-top-btn">Explore</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+        <div class="mhacks-top-bg"></div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+</script>
+<style src="../../assets/css/welcome.css" scoped>
+</style>
