d28d7c0ac9fc48945f96590aca029b07e8ada8f0
ajomadlabs <ajojohn@madlabs.xyz>
2018-07-13 22:11:11 +0530

    Completed the initial layout :smiley: :tada:

--- src/components/pageComponents/Welcome.vue
+++ src/components/pageComponents/Welcome.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
+    <!-- Top Section -->
     <div class="row">
+      <!-- Top Nav, Top Header and Top Description -->
       <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
         <div class="container">
           <ul class="mhack-top-nav">
@@ -8,13 +10,16 @@
               <a>Mad.Hacks</a>
             </li>
             <li class="mhacks-tn-right-item">
-              <a>Contact Us</a>
+              <a>Contact</a>
             </li>
             <li class="mhacks-tn-right-item">
-              <a>About Us</a>
+              <a>About</a>
             </li>
             <li class="mhacks-tn-right-item">
-              <a>Services</a>
+              <a>Business</a>
+            </li>
+            <li class="mhacks-tn-right-item">
+              <a>Students</a>
             </li>
           </ul>
         </div>
@@ -22,7 +27,7 @@
           <div class="container">
             <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
               <div class="mhacks-top-header">
-                Community For All
+                Student Developer Community
               </div>
             </div>
           </div>
@@ -31,8 +36,8 @@
           <div class="container">
             <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
               <div class="mhacks-top-para">
-                Student developer community built for and by the students to
-                enhance the skillset of students as well as to develop tech savy tools.
+                A community built for and by the students that develops
+                quality products for business.
               </div>
             </div>
           </div>
@@ -45,11 +50,331 @@
           </div>
         </div>
       </div>
+      <!-- End -->
+      <!-- Top Background Image -->
       <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
         <div class="mhacks-top-bg"></div>
       </div>
+      <!-- End -->
     </div>
+    <!-- End -->
+    <!-- Student Section -->
+    <div class="row">
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <div class="mhacks-mid-bg">
+          <center>
+            <div class="mhacks-mid-header">Are you a Student ?</div>
+          </center>
+        </div>
+      </div>
+    </div>
+    <!-- End -->
+    <!-- Student Content -->
+    <div class="container">
+      <div class="row">
+        <!-- Content Section -->
+        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          <div class="mhacks-mid-content">
+            Attractive real-world projects to be done.<br>
+            Learn Industry Practices.<br>
+            Enhance skills doing real-world projects.<br>
+            Earn rewards based on work done.<br>
+          </div>
+        </div>
+        <!-- End -->
+        <!-- Image Section -->
+        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          <img class="mhacks-mid-content-pic" src="../../assets/images/stud.svg"/>
+        </div>
+        <!-- End -->
+      </div>
+    </div>
+    <!-- End -->
+    <!-- Features Section -->
+    <div class="row">
+      <!-- Image Section -->
+      <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+        <div class="mhacks-mid-ft"></div>
+      </div>
+      <!-- End -->
+      <!-- Title Section -->
+      <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+        <div class="mhacks-mid-ft-content">
+          <div class="mhacks-mid-ft-text">Features</div>
+        </div>
+      </div>
+      <!-- End -->
+    </div>
+    <!-- End -->
+    <!-- Features Images Section -->
+    <div class="container mhacks-mid-container">
+      <div class="row">
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+          <img class="mhacks-mid-ft-list" src="../../assets/images/codeq.svg">
+        </div>
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+          <img class="mhacks-mid-ft-list" src="../../assets/images/respon.svg">
+        </div>
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+          <img class="mhacks-mid-ft-list" src="../../assets/images/browser.svg">
+        </div>
+      </div>
+    </div>
+    <!-- End -->
+    <!-- Features Images Title Section -->
+    <div class="container mhacks-mid-container">
+      <div class="row">
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+          <div class="mhacks-mid-ft-list-name">Code Quality</div>
+        </div>
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+          <div class="mhacks-mid-ft-list-name">Responsive</div>
+        </div>
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+          <div class="mhacks-mid-ft-list-name">Browser</div>
+        </div>
+      </div>
+    </div>
+    <!-- End -->
+    <!-- Features Images Description Section -->
+    <div class="container mhacks-mid-container">
+      <div class="row">
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+          <div class="mhacks-mid-ft-list-para">
+            Latest technology used.<br>
+            Neat and clean code.<br>
+            Best Industry practices.<br>
+          </div>
+        </div>
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+          <div class="mhacks-mid-ft-list-para">
+            Responsivenes at its best.<br>
+            All device compatible.<br>
+            Clear out clarity.<br>
+          </div>
+        </div>
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+          <div class="mhacks-mid-ft-list-para">
+            Runs on any browser.<br>
+            Supports every screen size.<br>
+            Easily viewable.<br>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- End -->
+    <!-- Features Images Section -->
+    <div class="container mhacks-mid-container">
+      <div class="row">
+        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          <img class="mhacks-mid-ft-list" src="../../assets/images/tech.svg">
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          <img class="mhacks-mid-ft-list" src="../../assets/images/life.svg">
+        </div>
+      </div>
+    </div>
+    <!-- End -->
+    <!-- Features Images Tiltle Section -->
+    <div class="container mhacks-mid-container">
+      <div class="row">
+        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          <div class="mhacks-mid-ft-list-name">Technical Support</div>
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          <div class="mhacks-mid-ft-list-name">Lifetime Support</div>
+        </div>
+      </div>
+    </div>
+    <!-- End -->
+    <!-- Features Images Description Section -->
+    <div class="container mhacks-mid-container">
+      <div class="row">
+        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          <div class="mhacks-mid-ft-list-two-para">
+            Technical assistance available.<br>
+            Schedule maintainence available.<br>
+            Super fast maintainence.<br>
+          </div>
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          <div class="mhacks-mid-ft-list-two-para">
+            Life time support available.<br>
+            Frequent updation.<br>
+            Later stage addition.<br>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- End -->
+    <!-- Mission Section -->
+    <div class="row">
+      <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+        <!-- Mission Title -->
+        <div class="row">
+          <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <div class="mhacks-mid-mission-bg">
+              <div class="mhacks-mid-mission-text">Our Mission</div>
+            </div>
+          </div>
+        </div>
+        <!-- End -->
+        <!-- Mission Description -->
+        <div class="container mhacks-mid-container">
+          <div class="row">
+            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+              <div class="mhacks-mid-mission-content">
+                MadHacks aims to support both students and Business in
+                each and every way it can. We help students to gain the
+                industrial knowledge by providing them with real world
+                projects. We also ensure that the business we collaborate
+                will get quality tech products with the use of latest
+                technology.
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- End -->
+      </div>
+      <!-- End -->
+      <!-- Mission Section Background -->
+      <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+        <div class="mhacks-mid-mission-sec-bg"></div>
+      </div>
+      <!-- End -->
+    </div>
+    <!-- End -->
+    <!-- Team Section -->
+    <div class="row">
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <div class="mhacks-mid-team-header">Team</div>
+      </div>
+    </div>
+    <!-- End -->
+    <!-- Team Section Content -->
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+          <div class="mhacks-mid-team-card">
+            <div class="row">
+              <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <center>
+                  <img class="mhacks-mid-team-card-pic" src="../../assets/images/alan.jpeg"/>
+                </center>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <center>
+                  <div class="mhacks-mid-team-card-pic-name">Alan Mathew</div>
+                </center>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <center>
+                  <div class="mhacks-mid-team-card-pic-position">Back-End Dev</div>
+                </center>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <center>
+                  <div class="mhacks-mid-team-card-pic-quote">
+                    <i class="fas fa-quote-left"></i>
+                      Our business relies on fast and reliable talent and support.<br>
+                      Some sort of relevant text appear here.
+                    <i class="fas fa-quote-right"></i>
+                  </div>
+                </center>
+              </div>
+            </div>
+            <div class="container mhacks-mid-team-container">
+              <center>
+              <div class="row">
+                <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1">
+                  <img src="../../assets/images/alan.jpeg" class="mhacks-mid-team-card-other-pic"/>
+                </div>
+                <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1">
+                  <img src="../../assets/images/dani.jpeg" class="mhacks-mid-team-card-other-pic"/>
+                </div>
+                <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1">
+                  <img src="../../assets/images/james.jpeg" class="mhacks-mid-team-card-other-pic"/>
+                </div>
+                <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1">
+                  <img src="../../assets/images/kavi.jpeg" class="mhacks-mid-team-card-other-pic"/>
+                </div>
+              </div>
+              </center>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- End -->
+    <!-- Hire Us Now Section -->
+    <div class="row">
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <div class="mhacks-last-contact-bg">
+          <div class="mhacks-last-contact-header">Hire us Now</div>
+        </div>
+      </div>
+    </div>
+    <!-- End -->
+    <!-- Contact Us Section -->
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+              <div class="mhacks-last-contact-input-header">Drop us a message</div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+              <input class="mhacks-last-contact-input" type="text" placeholder="Name"/>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+              <input class="mhacks-last-contact-input" type="text" placeholder="Email"/>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+              <input class="mhacks-last-contact-input" type="text" placeholder="Contact No"/>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+              <textarea class="mhacks-last-contact-textarea">Message</textarea>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+              <div class="mhacks-last-contact-btn">Submit</div>
+            </div>
+          </div>
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+              <div class="mhacks-last-contact-input-bg"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- End -->
+    <!-- Footer Section -->
+    <div class="row">
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <div class="mhacks-last-footer-bg"></div>
+      </div>
+    </div>
+    <!-- End -->
   </div>
+  <!-- End -->
 </template>
 <script>
 </script>
