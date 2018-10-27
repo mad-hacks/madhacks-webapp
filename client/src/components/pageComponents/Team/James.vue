c4e22c4db5cb490c064ea480dc5ea8b7258eb0b0
jamesgeorge007 <jamesgeorge998001@gmail.com>
2018-10-28 00:24:49 +0530

    Change pics and other workarounds :construction:

--- client/src/components/pageComponents/Team/James.vue
+++ client/src/components/pageComponents/Team/James.vue
@@ -3,7 +3,7 @@
             <div class="row">
               <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                 <center>
-                  <img class="mhacks-mid-team-card-pic" src="../../../assets/images/james.jpeg"/>
+                  <img class="mhacks-mid-team-card-pic" src="../../../assets/images/james.jpg"/>
                 </center>
               </div>
             </div>
