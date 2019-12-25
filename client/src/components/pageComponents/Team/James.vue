8246c7f60a412630679279ad0d8e8b28d27eca71
James George <jamesgeorge998001@gmail.com>
2019-12-26 00:09:39 +0530

    fix: typo

--- client/src/components/pageComponents/Team/James.vue
+++ client/src/components/pageComponents/Team/James.vue
@@ -17,7 +17,7 @@
             <div class="row">
               <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                 <center>
-                  <div class="mhacks-mid-team-card-pic-position">Techy</div>
+                  <div class="mhacks-mid-team-card-pic-position">Techie</div>
                 </center>
               </div>
             </div>
